838d4336c5193da723645e99d1e2831df60c629c
Faridho <faridho@zeepos.com>
2020-03-25 18:02:19 +0700

    full code

--- BirthdayNoteTaker/ViewController.swift
+++ BirthdayNoteTaker/ViewController.swift
@@ -9,12 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var birthdayTextField: UITextField!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var birthdayLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //call stored from database
+        let storedName = UserDefaults.standard.object(forKey: "name")
+        let storedBirthday = UserDefaults.standard.object(forKey: "birthday")
+        
+        //Casting - as? vs as!
+        if let newName = storedName as? String {
+            nameLabel.text = "Name: \(newName)"
+        }
+        
+        if let newbBirthday = storedBirthday as? String {
+            birthdayLabel.text = "Birthday: \(newbBirthday)"
+        }
+        
     }
 
 
+    @IBAction func saveClicked(_ sender: Any) {
+        //store database
+        UserDefaults.standard.set(nameTextField.text!, forKey: "name")
+        UserDefaults.standard.set(birthdayTextField.text!, forKey: "birthday")
+        
+        nameLabel.text = "Name: \(nameTextField.text!)"
+        birthdayLabel.text = "Birthday: \(birthdayTextField.text!)"
+    }
+    
+    
+    @IBAction func deleteClicked(_ sender: Any) {
+        let storedName = UserDefaults.standard.object(forKey: "name")
+        let storedBirthday = UserDefaults.standard.object(forKey: "birthday")
+        
+        //"" vs nill
+        if (storedName as? String) != nil {
+            UserDefaults.standard.removeObject(forKey: "name")
+            nameLabel.text = "Name: "
+        }
+        
+        if (storedBirthday as? String) != nil {
+            UserDefaults.standard.removeObject(forKey: "birthday")
+            birthdayLabel.text = "Birthday: "
+        }
+    }
 }
 
